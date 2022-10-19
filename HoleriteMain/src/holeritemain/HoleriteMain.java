e440b38dfc2445de58da2963a569be802b13f2b2
André de Queiroz <andrepatrinicola@gmail.com>
2022-10-18 23:50:43 -0300

    adiciona classe de menu

--- HoleriteMain/src/holeritemain/HoleriteMain.java
+++ HoleriteMain/src/holeritemain/HoleriteMain.java
@@ -1,20 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package holeritemain;
 
-/**
- *
- * @author andre
- */
-public class HoleriteMain {
+import menu.Menu;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class HoleriteMain 
+{
+
+    public static void main(String[] args) 
+    {
+        
+        Menu.menu();
+        
     }
     
 }
---
+++ HoleriteMain/src/menu/Menu.java
@@ -0,0 +1,24 @@
+package menu;
+
+
+public class Menu 
+{
+    
+    public static void menu()
+    {
+        
+        System.out.println("#############################################");
+        System.out.println("#########  CALCULADORA DE HOLERITE  #########");
+        System.out.println("#############################################\n");
+        
+        System.out.println("#############################################");
+        System.out.println("## Escolha uma das opções para continuar:  ##");
+        System.out.println("## 1 - Calcular Holerite                   ##");
+        System.out.println("## 2 - Selecionar funcionário              ##");
+        System.out.println("## 3 - Listar todos holerites              ##");
+        System.out.println("## 4 - Sair                                ##");
+        System.out.println("#############################################");
+        
+    }
+    
+}
